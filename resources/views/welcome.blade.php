a09948c5f962fbc12ec11a1e5d1ed342e4f2d708
Gerd Ratsch <ratsch.gerd@gmail.com>
2019-11-16 20:02:56 +0100

    Import branch test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Test version.
+                    Laravel Test PHP-2030 version.
                 </div>
 
                 <div class="links">
